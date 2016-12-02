0df86a02b77fd0092398397b22c9cb5d2c2531b0
Stefaan Avonds <Stefaan.Avonds@hotmail.com>
2016-12-02 16:39:03 +0100

    AssemblyInfo

--- Encryptors/Properties/AssemblyInfo.cs
+++ Encryptors/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Encryptors")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An application to encrypt a text using any encryptor.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stefaan Avonds")]
 [assembly: AssemblyProduct("Encryptors")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
